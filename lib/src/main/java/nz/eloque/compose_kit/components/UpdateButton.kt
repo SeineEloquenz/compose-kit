36ab908ac5cb1deb02c8c5f50951b98e7e4163a8
JSchmerling <mail@schmerl.ing>
2026-06-11 14:23:43 +0200

    Add import for resource in UpdateButton.kt

--- lib/src/main/java/nz/eloque/compose_kit/components/UpdateButton.kt
+++ lib/src/main/java/nz/eloque/compose_kit/components/UpdateButton.kt
@@ -15,6 +15,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.res.stringResource
+import nz.eloque.compose_kit.R
 
 @Composable
 fun UpdateButton(
