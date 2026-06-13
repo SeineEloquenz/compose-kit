1b6ee29f18c7ba4f057214f90a788242f68bed00
JSchmerling <mail@schmerl.ing>
2026-06-13 19:13:31 +0200

    Remove unused import for stringResource

--- lib/src/commonMain/kotlin/nz/eloque/compose_kit/input/SimpleTextField.kt
+++ lib/src/commonMain/kotlin/nz/eloque/compose_kit/input/SimpleTextField.kt
@@ -18,7 +18,6 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.res.stringResource
 import nz.eloque.compose_kit.resources.Res
 import nz.eloque.compose_kit.resources.compose_kit_invalid_input
 import org.jetbrains.compose.resources.stringResource
