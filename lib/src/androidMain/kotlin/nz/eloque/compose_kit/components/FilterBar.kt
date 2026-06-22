96cb0e09e45949562ac634e16ce8d761c5c2840f
JSchmerling <mail@schmerl.ing>
2026-06-22 19:04:13 +0200

    Refactor FilterBar to manage focus with LaunchedEffect

--- lib/src/androidMain/kotlin/nz/eloque/compose_kit/components/FilterBar.kt
+++ lib/src/androidMain/kotlin/nz/eloque/compose_kit/components/FilterBar.kt
@@ -1,6 +1,5 @@
 package nz.eloque.compose_kit.components
 
-import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.material.icons.Icons
@@ -13,15 +12,16 @@ import androidx.compose.material3.SearchBar
 import androidx.compose.material3.SearchBarDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.onFocusChanged
-import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.unit.dp
+import nz.eloque.compose_kit.internal.shouldClearFocusOnImeHide
 import nz.eloque.compose_kit.resources.Res
 import nz.eloque.compose_kit.resources.compose_kit_delete
 import nz.eloque.compose_kit.resources.compose_kit_search
@@ -38,8 +38,15 @@ fun FilterBar(
     val focusManager = LocalFocusManager.current
     var isFocused by rememberSaveable { mutableStateOf(false) }
     var query by rememberSaveable { mutableStateOf("") }
+    val shouldClear = shouldClearFocusOnImeHide()
 
-    BackHandler(enabled = isFocused) { focusManager.clearFocus() }
+    LaunchedEffect(shouldClear) {
+        if (shouldClear) {
+            isFocused = false
+            focusManager.clearFocus()
+        }
+    }
+
     SearchBar(
         windowInsets = WindowInsets(0.dp),
         inputField = {
@@ -61,10 +68,9 @@ fun FilterBar(
                 },
                 onExpandedChange = {},
                 expanded = false,
-                modifier =
-                    Modifier
-                        .fillMaxHeight()
-                        .onFocusChanged { isFocused = it.isFocused },
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .onFocusChanged { isFocused = it.isFocused },
                 trailingIcon = {
                     if (query.isNotEmpty()) {
                         IconButton(
