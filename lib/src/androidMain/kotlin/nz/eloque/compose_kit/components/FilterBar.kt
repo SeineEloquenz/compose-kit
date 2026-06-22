26a4997c95953f0f2577ed41f67eff67df8bdcdf
JSchmerling <mail@schmerl.ing>
2026-06-22 18:51:27 +0200

    Refactor FilterBar for Android with BackHandler

--- lib/src/androidMain/kotlin/nz/eloque/compose_kit/components/FilterBar.kt
+++ lib/src/androidMain/kotlin/nz/eloque/compose_kit/components/FilterBar.kt
@@ -1,5 +1,6 @@
 package nz.eloque.compose_kit.components
 
+import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.material.icons.Icons
@@ -12,7 +13,6 @@ import androidx.compose.material3.SearchBar
 import androidx.compose.material3.SearchBarDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
@@ -20,15 +20,12 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import nz.eloque.compose_kit.resources.Res
 import nz.eloque.compose_kit.resources.compose_kit_delete
 import nz.eloque.compose_kit.resources.compose_kit_search
 import org.jetbrains.compose.resources.stringResource
-import org.jetbrains.compose.ui.tooling.preview.Preview
-
-@Composable
-expect fun isImeVisible(): Boolean
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -39,15 +36,8 @@ fun FilterBar(
     val focusManager = LocalFocusManager.current
     var isFocused by rememberSaveable { mutableStateOf(false) }
     var query by rememberSaveable { mutableStateOf("") }
-    val imeVisible = isImeVisible()
 
-    LaunchedEffect(imeVisible) {
-        if (!imeVisible) {
-            isFocused = false
-            focusManager.clearFocus()
-        }
-    }
-
+    BackHandler(enabled = isFocused) { focusManager.clearFocus() }
     SearchBar(
         windowInsets = WindowInsets(0.dp),
         inputField = {
@@ -69,9 +59,10 @@ fun FilterBar(
                 },
                 onExpandedChange = {},
                 expanded = false,
-                modifier = Modifier
-                    .fillMaxHeight()
-                    .onFocusChanged { isFocused = it.isFocused },
+                modifier =
+                    Modifier
+                        .fillMaxHeight()
+                        .onFocusChanged { isFocused = it.isFocused },
                 trailingIcon = {
                     if (query.isNotEmpty()) {
                         IconButton(
@@ -80,10 +71,7 @@ fun FilterBar(
                                 onSearch.invoke("")
                             },
                         ) {
-                            Icon(
-                                imageVector = Icons.Default.Clear,
-                                contentDescription = stringResource(Res.string.compose_kit_delete),
-                            )
+                            Icon(imageVector = Icons.Default.Clear, contentDescription = stringResource(Res.string.compose_kit_delete))
                         }
                     }
                 },
@@ -92,7 +80,8 @@ fun FilterBar(
         expanded = false,
         onExpandedChange = {},
         modifier = modifier,
-    ) {}
+    ) {
+    }
 }
 
 @Preview
